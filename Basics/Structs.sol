6fba172d2fc7fbc3ef25b32ca7824d1896fcf249
Devansh <58616444+Devansh3712@users.noreply.github.com>
2021-04-30 13:00:26 +0530

    Update Structs.sol

--- Basics/Structs.sol
+++ Basics/Structs.sol
@@ -3,20 +3,18 @@ pragma solidity ^0.8.4;
 
 contract Structs {
     /*
-    State variable =>
-    
-    scope of variable is global
-    (can be used in the whole program)
-    */
+     * State variable =>
+     * scope of variable is global
+     * (can be used in the whole program)
+     */
     uint public myUint = 1; // uint => unsigned integer (positive values)
     address public myAddress = 0x4C501d78c6Bf497d276dcB2D6aDD66B351CcDb85; // ethereum address
     string public myString = "Hello World!";
     
     /*
-    Struct =>
-    
-    create custom data structures
-    */
+     * Struct =>
+     * create custom data structures
+     */
     struct MyStruct {
         address id;
         uint amount;
@@ -25,11 +23,10 @@ contract Structs {
     MyStruct public myStruct = MyStruct(0x4C501d78c6Bf497d276dcB2D6aDD66B351CcDb85, 1);
     
     /*
-    local variable =>
-
-    scope of variable is only inside the
-    function
-    */
+     * local variable =>
+     * scope of variable is only inside the
+     * function
+     */
     function getValue() public pure returns(uint) {
         uint value = 1;
         return value;
